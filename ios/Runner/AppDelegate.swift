d8d0a537670636026729eae21462a82fca2d9591
BilgeAS <admin@bilgeas.no>
2025-12-22 22:28:43 +0100

    opdate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,24 +1,17 @@
-import Flutter
 import UIKit
-import GoogleMaps // Make sure this line exists
+import Flutter
+import GoogleMaps
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
-  var flutterEngine: FlutterEngine?
-
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    // Keep your Google Maps API key registration
+    // Registrer Google Maps API-nøkkel
     GMSServices.provideAPIKey("AIzaSyAOJtMeKdyPm2z083yGT78nAj1lfzPdtmM")
 
-    // Initialize a shared FlutterEngine so SceneDelegate can create Flutter views
-    flutterEngine = FlutterEngine(name: "my_engine")
-    flutterEngine?.run()
-    if let engine = flutterEngine {
-      GeneratedPluginRegistrant.register(with: engine)
-    }
+    GeneratedPluginRegistrant.register(with: self)
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
